1a5fe879a4688d30b692c87c83d6dff1eb84d266
Genn <intval@i.ua>
2022-02-16 10:31:44 +0200

    start

--- js/main.js
+++ js/main.js
@@ -1,33 +1,60 @@
 "use strict";
 let shch = {};
 shch.topSlider = function () {
-    this.number = 1;
-    this.prev = 1;
-    this.current = document.querySelector('.current')
-    this.changeTag = document.querySelector('.headBack');
+    this.number = 0;
+    this.current = document.querySelector('.current');
+    this.changeMP = document.querySelectorAll('.mainProposition');
+    this.changeTitle = document.querySelectorAll('.headMain');
+    this.changeAction = document.querySelectorAll('.actionMain');
     this.changeBack = document.querySelectorAll('.headBack');
-    this.setA = document.querySelectorAll('.arrowRight');
-    this.beforeAct = '';
-    this.act = '';
+    this.changeSI = document.querySelectorAll('.SliderItem');
+    this.slide0 = 0;
+    this.slide1 = 1;
     this.less = function () {
-        this.changeBack[this.number - 1].classList.remove('actBack');
-        this.prev = this.number;
+        this.changeMP[this.number].classList.remove('mpAct');
+        this.changeTitle[this.number].classList.remove('actTitle');
+        this.changeAction[this.number].classList.remove('actM');
+        this.changeBack[this.number].classList.remove('actBack');
+        this.changeSI[this.slide0].classList.remove('SI' + this.slide0 + 'act');
+        this.changeSI[this.slide1].classList.remove('SI' + this.slide1 + 'act');
         this.number--;
-        if (this.number < 1) this.number = 1;
-        this.changeBack[this.number - 1].classList.add('actBack');
-        this.changeTag.classList.remove('headBack' + this.prev);
-        this.changeTag.classList.add('headBack' + this.number);
-        this.current.textContent = this.number;
+        this.slide0--;
+        this.slide1--;
+        if (this.number < 0) {
+            this.number = 0;
+            this.slide0 = 0;
+            this.slide1 = 1;
+        }
+        this.changeMP[this.number].classList.add('mpAct');
+        this.changeTitle[this.number].classList.add('actTitle');
+        this.changeAction[this.number].classList.add('actM');
+        this.changeBack[this.number].classList.add('actBack');
+        this.changeSI[this.slide0].classList.add('SI' + this.slide0 + 'act');
+        this.changeSI[this.slide1].classList.add('SI' + this.slide1 + 'act');
+        this.current.textContent = this.number + 1;
     };
     this.more = function () {
-        this.changeBack[this.number + 1].classList.remove('actBack');
-        this.prev = this.number;
+        this.changeMP[this.number].classList.remove('mpAct');
+        this.changeTitle[this.number].classList.remove('actTitle');
+        this.changeAction[this.number].classList.remove('actM');
+        this.changeBack[this.number].classList.remove('actBack');
+        this.changeSI[this.slide0].classList.remove('SI' + this.slide0  + 'act');
+        this.changeSI[this.slide1].classList.remove('SI' + this.slide1 + 'act');
         this.number++;
-        if (this.number > 5) this.number = 5;
-        this.changeBack[this.number + 1].classList.add('actBack');
-        this.changeTag.classList.remove('headBack' + this.prev);
-        this.changeTag.classList.add('headBack' + this.number);
-        this.current.textContent = this.number;
+        this.slide0++;
+        this.slide1++;
+        if (this.number > 4) {
+            this.number = 4;
+            this.slide0 = 3;
+            this.slide1 = 4;
+        }
+        this.changeMP[this.number].classList.add('mpAct');
+        this.changeTitle[this.number].classList.add('actTitle');
+        this.changeAction[this.number].classList.add('actM');
+        this.changeBack[this.number].classList.add('actBack');
+        this.changeSI[this.slide0].classList.add('SI' + this.number + 'act');
+        this.changeSI[this.slide1].classList.add('SI' + (this.number + 1) + 'act');
+        this.current.textContent = this.number + 1;
     };
 };
 
