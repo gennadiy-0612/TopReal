0270505a9968b42f00d3ba5e54b9ac0799f08f16
Genn <intval@i.ua>
2022-02-22 19:29:22 +0200

    work-version

--- js/main.js
+++ js/main.js
@@ -91,9 +91,6 @@ shch.secSlide = function (s) {
     this.slideSecB = document.querySelectorAll('.photoBack');
     this.slideSecMemoB = document.querySelectorAll('.photoBack')[0];
     this.slideSecMemo1B = document.querySelectorAll('.photoBack')[1];
-
-    this.memo0 = 0;
-    this.memo1 = 1;
     this.changeIt = function (n) {
         if (this.slideSecMemo !== this.slideSec[n]) {
             this.changerMemo.classList.remove('VividBrownTrapezoid');
@@ -103,6 +100,13 @@ shch.secSlide = function (s) {
             this.slideSecMemo.classList.remove('photoAct', 'PA' + this.memo0);
             this.slideSecMemo.classList.add('photoNoAct', 'PNA' + this.memo0);
             this.slideSecMemo1.classList.remove('photoNoAct', 'PNA' + this.memo1);
+            if (this.memo1 === n) {
+                this.slideSecMemo.classList.remove('equal');
+                this.slideSecMemo1.classList.add('equal');
+            } else {
+                this.slideSecMemo.classList.remove('equal');
+                this.slideSecMemo1.classList.remove('equal');
+            }
             this.slideSecMemo1 = this.slideSecMemo;
             this.memo1 = this.memo0;
             this.memo0 = n;
