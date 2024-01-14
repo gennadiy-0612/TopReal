d0c98995cd2ede84f2f79ca6a4eee816ff5a8e9d
user <fotoxsppua@gmail.com>
2024-01-14 15:07:04 +0200

    backChange

--- js/main.js
+++ js/main.js
@@ -35,8 +35,8 @@ shch.topSlider = function () {
         this.changeAction[this.number].classList.add('actM');
         this.changeBack[this.number].classList.add('actBack');
         if (-1 < this.slide0 && this.slide0 < 5) {
-            this.changeSI[this.slide0].classList.add('SI' + this.slide0 + 'act', 'prevSlide', 'backFS');
-            this.SliderBack[this.slide0].classList.add('SB' + this.slide0 + 'actSB', 'prevSlideSB', 'backBS');
+            this.changeSI[this.slide0].classList.add('SI' + this.slide0 + 'act', 'prevSlide', 'prevBackFS');
+            this.SliderBack[this.slide0].classList.add('SB' + this.slide0 + 'actSB', 'prevSlideSB', 'prevBackBS');
         }
         if (-1 < this.slide1 && this.slide1 < 5) {
             this.changeSI[this.slide1].classList.add('SI' + this.slide1 + 'act', 'currentSlide', 'currentFS');
