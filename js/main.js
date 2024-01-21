2353ce74c277b59e8be37aff01f78ff0e51fc2d9
user <fotoxsppua@gmail.com>
2024-01-21 12:32:02 +0200

    start

--- js/main.js
+++ js/main.js
@@ -35,12 +35,12 @@ shch.topSlider = function () {
         this.changeAction[this.number].classList.add('actM');
         this.changeBack[this.number].classList.add('actBack');
         if (-1 < this.slide0 && this.slide0 < 5) {
-            this.changeSI[this.slide0].classList.add('SI' + this.slide0 + 'act', 'prevSlide');
-            this.SliderBack[this.slide0].classList.add('SB' + this.slide0 + 'actSB', 'prevSlideSB');
+            this.changeSI[this.slide0].classList.add('SI' + this.slide0 + 'act', 'prevSlide', 'prevSlideToBack');
+            this.SliderBack[this.slide0].classList.add('SB' + this.slide0 + 'actSB', 'prevSlideSB', 'prevSlideSBToBack');
         }
         if (-1 < this.slide1 && this.slide1 < 5) {
-            this.changeSI[this.slide1].classList.add('SI' + this.slide1 + 'act', 'currentSlide');
-            this.SliderBack[this.slide1].classList.add('SB' + this.slide1 + 'actSB', 'currentSlideSB');
+            this.changeSI[this.slide1].classList.add('SI' + this.slide1 + 'act', 'currentSlide', 'currentSlideToBack');
+            this.SliderBack[this.slide1].classList.add('SB' + this.slide1 + 'actSB', 'currentSlideSB', 'currentSlideSBToBack');
         }
         this.current.textContent = this.number + 1;
     };
